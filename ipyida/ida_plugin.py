e50db60e1a7c35d6c882373dcd3a65a809429f5b
Marc-Etienne M.Léveillé <leveille@eset.com>
2016-05-27 10:42:08 -0400

    Fix loading of the plugin if IDB is openned on Windows

--- ipyida/ida_plugin.py
+++ ipyida/ida_plugin.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf8 -*-
 #
-# IDA plugin definition
+# IDA plugin definition.
 #
 # Copyright (c) 2015 ESET
 # Author: Marc-Etienne M.Léveillé <leveille@eset.com>
@@ -25,7 +25,6 @@ class IPyIDAPlugIn(idaapi.plugin_t):
     def run(self, args):
         if not self.kernel.started:
             self.kernel.start()
-            self.widget = qtconsole.IPythonConsole(self.kernel.connection_file)
         if self.widget is None:
             self.widget = qtconsole.IPythonConsole(self.kernel.connection_file)
         self.widget.Show()
@@ -44,20 +43,32 @@ def PLUGIN_ENTRY():
 _kernel = kernel.IPythonKernel()
 _kernel.start()
 
-def load():
+def _do_load():
     ipyida_plugin_path = __file__
     if ipyida_plugin_path.endswith("pyc"):
         # IDA Python can't load pyc, only the Python source so we remove the "c"
         ipyida_plugin_path = ipyida_plugin_path[:-1]
     idaapi.load_plugin(ipyida_plugin_path)
 
-def install():
-    def handler(event, old=0):
-        if event == idaapi.NW_OPENIDB:
-            load()
-        elif event == idaapi.NW_TERMIDA:
-            idaapi.notify_when(idaapi.NW_TERMIDA | idaapi.NW_OPENIDB | idaapi.NW_REMOVE, handler)
-    def _install():
-        idaapi.notify_when(idaapi.NW_TERMIDA | idaapi.NW_OPENIDB, handler)
-        return -1
-    idaapi.register_timer(1000, _install)
+def load():
+    """
+    Perform necessary steps to load the plugin inside IDA. If no IDB is open, it
+    will wait until it is open to load it.
+    """
+    if idaapi.get_root_filename() is None:
+        # No idb open yet
+        def handler(event, old=0):
+            if event == idaapi.NW_OPENIDB:
+                _do_load()
+            elif event == idaapi.NW_TERMIDA:
+                idaapi.notify_when(idaapi.NW_TERMIDA | idaapi.NW_OPENIDB | idaapi.NW_REMOVE, handler)
+        def _install():
+            idaapi.notify_when(idaapi.NW_TERMIDA | idaapi.NW_OPENIDB, handler)
+            # return -1 to remove the timer
+            return -1
+        # It's possible we can't use the notify_when API call yet when IDA opens
+        # so try register a timer to add the event listner in the proper "state"
+        idaapi.register_timer(1, _install)
+    else:
+        # IDA is fully loaded and an idb is open, just load the plugin.
+        _do_load()
