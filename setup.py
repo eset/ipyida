2aa6540821278903d9e0e26ee0326d8719356a34
Marc-Etienne M.Léveillé <leveille@eset.com>
2016-05-27 10:45:19 -0400

    Fix description and add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
+# -*- encoding: utf8 -*-
+#
+# Copyright (c) 2016 ESET
+# Author: Marc-Etienne M.Léveillé <leveille@eset.com>
+# See LICENSE file for redistribution.
 
 from distutils.core import setup
 
-setup(name='IPyIDA',
+setup(name='ipyida',
       version='0.9',
-      description='IDA plugin to embed IPython console inside IDA',
+      description='IDA plugin to embed the IPython console inside IDA',
       author='Marc-Etienne M.Léveillé',
       author_email='leveille@eset.com',
       url='https://www.github.com/eset/ipyida',
       packages=['ipyida'],
+      install_requires=[
+          'ipython[qtconsole]==3.2.3',
+      ],
       license="BSD",
 )
