1607b40214cf4364836787a6682ea67a9a4f52a8
Marc-Etienne M.Léveillé <leveille@eset.com>
2016-05-27 10:52:37 -0400

    Add ipyida_plugin_stub.py that can be placed in IDA's plugins directory to install IPyIDA "the IDA way"

---
+++ ipyida_plugin_stub.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf8 -*-
+#
+# Simple stub to drop in IDA's "plugins" directory if you decide to use this
+# installation method. This file is not part of the pip package.
+#
+# Copyright (c) 2016 ESET
+# Author: Marc-Etienne M.Léveillé <leveille@eset.com>
+# See LICENSE file for redistribution.
+
+from ipyida.ida_plugin import PLUGIN_ENTRY, IPyIDAPlugIn
