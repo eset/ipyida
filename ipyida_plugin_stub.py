2b2cb360c00106caa2221beb57be80ee90f9e772
Marc-Etienne M.Léveillé <leveille@eset.com>
2018-06-26 14:39:01 -0400

    Remove comment in stub about alternate install method. Using the plugins directory will be the supported way soon.

--- ipyida_plugin_stub.py
+++ ipyida_plugin_stub.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf8 -*-
 #
-# Simple stub to drop in IDA's "plugins" directory if you decide to use this
-# installation method. This file is not part of the pip package.
+# Simple stub to drop in IDA's "plugins" directory.
 #
 # Copyright (c) 2016 ESET
 # Author: Marc-Etienne M.Léveillé <leveille@eset.com>
