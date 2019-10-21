333ab3ebd71c2405f5d7858ee4269962c850ae17
Marc-Etienne M.Léveillé <leveille@eset.com>
2019-10-21 10:52:01 -0400

    Use print function in ipyida_plugin_stub.py for Python 3 compatibility (#25)

--- ipyida/ipyida_plugin_stub.py
+++ ipyida/ipyida_plugin_stub.py
@@ -5,9 +5,10 @@
 # Copyright (c) 2016-2018 ESET
 # Author: Marc-Etienne M.Léveillé <leveille@eset.com>
 # See LICENSE file for redistribution.
+from __future__ import print_function
 
 try:
     from ipyida.ida_plugin import PLUGIN_ENTRY, IPyIDAPlugIn
 except ImportError:
-    print "[WARN] Could not load IPyIDA plugin. ipyida Python package " \
-          "doesn't seem to be installed."
+    print("[WARN] Could not load IPyIDA plugin. ipyida Python package " \
+          "doesn't seem to be installed.")
