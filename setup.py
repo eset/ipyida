36f96f1c8bc1a7471fcd494a4acdad3daaf8c2ed
Marc-Etienne M.Léveillé <leveille@eset.com>
2020-05-21 23:31:33 -0400

    Bump version to 1.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ See full README on GitHub: <https://www.github.com/eset/ipyida>.
 """
 
 setup(name='ipyida',
-      version='1.4',
+      version='1.5',
       description='IDA plugin to embed the IPython console inside IDA',
       long_description=long_description,
       long_description_content_type="text/markdown",
