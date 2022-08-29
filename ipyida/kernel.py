c649828926f07ac4e2363cf67b1d3b82e3014b2e
Marc-Etienne M.Léveillé <leveille@eset.com>
2022-08-29 09:12:37 -0400

    Display known name (function, variable, etc) and its offset if integer is in 'mapped' memory

--- ipyida/kernel.py
+++ ipyida/kernel.py
@@ -157,6 +157,15 @@ class IPythonKernel(object):
             printer.text(hex(obj))
         else:
             printer.text(str(obj))
+        info_struct = idaapi.get_inf_structure()
+        if obj >= info_struct.min_ea and obj < info_struct.max_ea:
+            addr = idaapi.prev_that(obj, info_struct.min_ea, idaapi.has_name)
+            if addr != idaapi.BADADDR:
+                name = idaapi.get_name(addr)
+                demangled = idaapi.demangle_name(name, 0)
+                if demangled and len(demangled) > 0:
+                    name = demangled
+                printer.text(" ({:s} + {:x})".format(name, obj - addr))
 
     @staticmethod
     def print_bytes(obj, printer, *args):
