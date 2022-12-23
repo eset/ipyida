4f715f0cf0fc25622b6c781244780fbfa72a5286
Marc-Etienne M.Léveillé <leveille@eset.com>
2022-12-23 13:25:07 -0500

    Don't end up in an infinite loop if notebook fails to start. Also display output so we can debug.

--- ipyida/notebook.py
+++ ipyida/notebook.py
@@ -135,11 +135,13 @@ class NotebookManager(object):
                 text=True
             )
             try_count = 0
-            while nb_server_info is None and try_count < 10:
+            while nb_server_info is None and self.nb_proc.poll() is None and try_count < 10:
                 time.sleep(0.5)
                 nb_server_info = self._get_running_notebook_config()
+                try_count += 1
             if nb_server_info is None:
                 self.nb_proc.terminate()
+                print(self.nb_proc.stdout.read())
                 raise Exception("Couldn't start Jupyter Notebook")
             else:
                 self.nb_pipe_thread = threading.Thread(target=self._notebook_stdout_thread)
